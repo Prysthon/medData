cbe17f3d73edc58718d9e7efebbb062069ba6a7e
Tiago Prysthon <tiagoprysthon14@gmail.com>
2025-04-30 19:35:12 -0300

    requests

---
+++ backend-mongo/requests/register_invalid_password.sh
@@ -0,0 +1,4 @@
+
+curl -X POST http://localhost:3000/register \
+     -H "Content-Type: application/json" \
+     -d '{"name": "Carlos", "email": "carlos@example.com", "password": "123"}' # Senha muito simples

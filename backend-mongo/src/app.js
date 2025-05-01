69a0dd1237b72eedae0470b098955760f6936790
Tiago Prysthon <tiagoprysthon14@gmail.com>
2025-04-30 22:35:30 -0300

    test

--- backend-mongo/src/app.js
+++ backend-mongo/src/app.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(express.json());
 
 // Rotas
-app.get('/test', (_request, response) => {
-  response.status(200).json('teste');
+app.get('/', (_request, response) => {
+  response.status(200).json('api funcionando');
 });
 app.use('/', authRoutes);
 app.use('/protected', protectedRoutes);

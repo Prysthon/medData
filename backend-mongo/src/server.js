955defb60102ea9a9df80cc5da608c6721922092
Tiago Prysthon <tiagoprysthon14@gmail.com>
2025-05-03 01:02:01 -0300

    fix: removing double db connection

--- backend-mongo/src/server.js
+++ backend-mongo/src/server.js
@@ -2,10 +2,6 @@
 require('dotenv').config();
 
 const app = require('./app');
-const connectDB = require('./database');
-
-// 2) conecta ao Mongo
-connectDB();
 
 // 3) inicia o servidor
 const PORT = process.env.PORT || 3000;
